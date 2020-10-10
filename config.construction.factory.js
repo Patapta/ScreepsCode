21a77ae28f538ffc7e994835b1fc75b15be88bf7
Aoi <Patapataboom@aliyun.com>
2020-10-10 16:08:33 +0800

    尝试修复傻逼他那卡因为不判断出现的问题

--- config.construction.factory.js
+++ config.construction.factory.js
@@ -2,34 +2,34 @@
  *   工厂工作配置文件
  *      参数：
  *          WorkFlag: 工厂是否工作
-*           Production: 工厂生产产品
-*           Interval: 工厂工作冷却时间
+ *          Production: 工厂生产产品
+ *          Interval: 工厂工作冷却时间
  *
  */
 module.exports = {
-    E6S22:({
-        WorkFlag: true,
-        Production: RESOURCE_ZYNTHIUM_BAR,
-        Interval: 21
-    }),
-    E9S21:({
-        WorkFlag: false,
-        Production: RESOURCE_BATTERY,
-        Interval: 11
-    }),
-    E8S23:({
-        WorkFlag: true,
-        Production: RESOURCE_REDUCTANT,
-        Interval: 21
-    }),
-    E8S25:({
-        WorkFlag: true,
-        Production: RESOURCE_LEMERGIUM_BAR,
-        Interval: 21
-    }),
-    E9S23:({
-        WorkFlag: true,
-        Production: RESOURCE_LEMERGIUM_BAR,
-        Interval: 21
-    }),
+    // E6S22:({
+    //     WorkFlag: true,
+    //     Production: RESOURCE_ZYNTHIUM_BAR,
+    //     Interval: 21
+    // }),
+    // E9S21:({
+    //     WorkFlag: false,
+    //     Production: RESOURCE_BATTERY,
+    //     Interval: 11
+    // }),
+    // E8S23:({
+    //     WorkFlag: true,
+    //     Production: RESOURCE_REDUCTANT,
+    //     Interval: 21
+    // }),
+    // E8S25:({
+    //     WorkFlag: true,
+    //     Production: RESOURCE_LEMERGIUM_BAR,
+    //     Interval: 21
+    // }),
+    // E9S23:({
+    //     WorkFlag: true,
+    //     Production: RESOURCE_LEMERGIUM_BAR,
+    //     Interval: 21
+    // }),
 }
